f61b346d86e00f53c8ca5c481dfed4d82069c865
alex <alex@iquea.es>
2025-06-19 21:26:13 +0200

    Hago el registro.html

--- bienvenido.php
+++ bienvenido.php
@@ -2,7 +2,7 @@
 // bienvenida.php
 session_start();
 if (!isset($_SESSION['username'])) {
-    header("Location: login.php");
+    header("Location: index.html");
     exit();
 }
 ?>
--- db.php
+++ db.php
@@ -1,13 +1,13 @@
 <?php
 // db.php
-$servidor = "localhost";
-$usuario = "root";
-$contrasena = "";
-$basededatos = "tu_base_de_datos";
+$servidor="localhost";
+$usuario="root";
+$contrasena="";
+$basededatos="tu_base_de_datos";
 
-$conexion = mysqli_connect($servidor, $usuario, $contrasena, $basededatos);
+$conexion=mysqli_connect($servidor, $usuario, $contrasena, $basededatos);
 
 if (!$conexion) {
-    die("La conexión ha fallado: " . mysqli_connect_error());
+    die("La conexión ha fallado: ".mysqli_connect_error());
 }
 ?>
