070a5b41a8e698f3afddab5c73aba56c8d7374aa
alex <alex@iquea.es>
2025-06-21 13:57:49 +0200

    pequeños ajustes de este archivo

--- registro.php
+++ registro.php
@@ -3,29 +3,27 @@
 require('db.php');
 
 if (isset($_POST['username']) && isset($_POST['password'])) {
-    $username = stripslashes($_POST['username']);
-    $username = mysqli_real_escape_string($conexion, $username);
-    $password = stripslashes($_POST['password']);
-    $password = mysqli_real_escape_string($conexion, $password);
+    $username=stripslashes($_POST['username']);
+    $username=mysqli_real_escape_string($conexion, $username);
+    $password=stripslashes($_POST['password']);
+    $password=mysqli_real_escape_string($conexion, $password);
 
-    // Encriptar la contraseña
-    $password_hash = password_hash($password, PASSWORD_DEFAULT);
+    $password_hash=password_hash($password, PASSWORD_DEFAULT);
 
-    // Insertar en la base de datos
-    $query = "INSERT INTO usuarios (username, password) VALUES ('$username', '$password_hash')";
-    $result = mysqli_query($conexion, $query);
+    $query="INSERT INTO usuarios (username, password) VALUES ('$username', '$password_hash')";
+    $result=mysqli_query($conexion, $query);
 
-    if ($result) {
+    if($result) {
         echo "<div class='form-container'>
                 <h3>Te has registrado correctamente.</h3>
                 <br/>Haz clic aquí para <a href='login.php'>Iniciar Sesión</a></div>";
-    } else {
+    }else{
         echo "<div class='form-container'>
                 <h3>Error al registrar. Intenta con otro nombre de usuario.</h3>
                 <br/><a href='registro.html'>Volver</a>
               </div>";
     }
-} else {
+}else{
 ?>
 <div class="form-container">
     <h3>Por favor, completa los campos.</h3>
